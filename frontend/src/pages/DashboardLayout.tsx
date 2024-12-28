36e2456164a1cbeffb6322aa4e2124acd0b0b3d3
Harshit Yadav <2105891@kiit.ac.in>
2024-12-29 01:04:59 +0530

    refactor: User image updated

--- frontend/src/pages/DashboardLayout.tsx
+++ frontend/src/pages/DashboardLayout.tsx
@@ -3,17 +3,27 @@ import { Separator } from "@/components/ui/separator";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 import { Outlet } from "react-router-dom";
-
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function DashboardLayout() {
+    const name = window.localStorage.getItem("loggedInUser");
     return (
         <SidebarProvider>
             <AppSidebar />
             <SidebarInset>
-                <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4 border-2 w-full">
-                    <SidebarTrigger className="-ml-1" />
-                    <Separator orientation="vertical" className="mr-2 h-4" />
-                    <h2>Create Links</h2>
+                <header className="flex h-16 shrink-0 items-center justify-between gap-2 border-b px-4 border-2 w-full">
+                    <div className="flex items-center gap-2">
+                        <SidebarTrigger className="-ml-1" />
+                        <Separator orientation="vertical" className="mr-2 h-4" />
+                        <h2>Create Links</h2>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <Avatar>
+                            <AvatarImage src="https://github.com/shadcn.png" />
+                            <AvatarFallback>{name}</AvatarFallback>
+                        </Avatar>
+                        <p>{name}</p>
+                    </div>
                 </header>
                 <Outlet />
             </SidebarInset>
